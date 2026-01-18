bd66a2c6b1322429c5a2938f5d007e87b3e97c14
ayman <aemanharde@gmail.log>
2026-01-18 21:32:47 +0300

    Seperate navBar to a component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
